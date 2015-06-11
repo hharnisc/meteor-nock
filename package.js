8425ab5651900d78f9627cced2c8450c40ea5b4f
Harrison Harnisch <hharnisc@gmail.com>
2015-06-10 20:39:49 -0700

    update package.js

--- package.js
+++ package.js
@@ -1,7 +1,9 @@
 Package.describe({
+  name: 'hharnisc:meteor-nock',
   summary: "Meteor smart-package for nock (https://github.com/pgte/nock)",
   version: "0.1.0",
   git: "https://github.com/hharnisc/meteor-nock.git",
+  documentation: 'README.md',
   debugOnly: true
 });
 
